4809d3eb9338130c0581dd39523a1fe55cfa0a1f
Anthony Li <me@anli.dev>
2024-12-03 20:16:38 -0500

    Slightly modify template

--- template.py
+++ template.py
@@ -1,15 +1,15 @@
 import sys
 import re
+from collections import *
 
 def count(it):
     return sum(1 for _ in it)
 
 lines = sys.stdin.read().split("\n")
 
-
-
 result = 0
 
-
+for line in lines:
+    pass
 
 print(result)
